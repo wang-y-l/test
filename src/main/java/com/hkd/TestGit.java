3dbca5d63aad8c421b3f07811488d4e78ba33097
yulong <yulong@hkd.com>
2022-06-27 20:37:08 +0800

    git 1

---
+++ src/main/java/com/hkd/TestGit.java
@@ -0,0 +1,11 @@
+package com.hkd;
+
+/**
+ * @Author WangYuLong
+ * @Date 2022/6/27
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("git 1");
+    }
+}
